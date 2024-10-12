a0a12a93f57c7ffc289e4ce9a279af579585631d
Vinay Rajagopal Menon <74170290+vrm-hub@users.noreply.github.com>
2024-10-12 17:40:52 -0400

    Time: 21 ms (10.9%), Space: 44.7 MB (20.17%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> charCount = new HashMap<>();
+        if(s.length() != t.length()) {
+            return false;
+        }
+        for(int i = 0; i < s.length(); i++) {
+            if(!charCount.containsKey(s.charAt(i))) {
+                charCount.put(s.charAt(i), 1);
+            } else {
+                int count = charCount.get(s.charAt(i));
+                count++;
+                charCount.put(s.charAt(i), count);
+            }
+        }
+
+        for(int i = 0; i < t.length(); i++) {
+            if(!charCount.containsKey(t.charAt(i))) {
+                return false;
+            } else {
+                int count = charCount.get(t.charAt(i));
+                count--;
+                if(count == 0) {
+                    charCount.remove(t.charAt(i));
+                } else {
+                    charCount.put(t.charAt(i), count);
+                }
+            }
+        }
+
+        return true;
+
+        // if(charCount.size() == 0) {
+        //     return true;
+        // } else {
+        //     for (Map.Entry<Character, Integer> set :
+        //      charCount.entrySet()) {
+ 
+        //     // Printing all elements of a Map
+        //     System.out.println(set.getKey() + " = "
+        //                        + set.getValue());
+        // }
+        //     return false;
+        // }
+    }
+}
