7916b682da73dffa74efb7dd7fdcd8302e93f671
Vinay Rajagopal Menon <74170290+vrm-hub@users.noreply.github.com>
2025-03-27 16:05:01 -0400

    Time: 8 ms (40.37%), Space: 47.5 MB (97.58%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -2,27 +2,24 @@ class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         HashMap<String, List<String>> map = new HashMap<>();
         for(int i = 0; i < strs.length; i++) {
-            char []arr = strs[i].toCharArray();
-            Arrays.sort(arr);
-            String str = String.valueOf(arr);
-            
+            char[] chars = strs[i].toCharArray();
+            Arrays.sort(chars);
+            String str = new String(chars);
             if(!map.containsKey(str)) {
-                List<String> anagramList = new ArrayList<String>();
-                anagramList.add(strs[i]);
-                map.put(str, anagramList);
+                map.put(str, new ArrayList<>(Arrays.asList(strs[i])));
             } else {
-                List<String> anagramList = map.get(str);
-                anagramList.add(strs[i]);
+                List<String> list = map.get(str);
+                list.add(strs[i]);
+                map.put(str, list);
             }
         }
 
-        List anagrams = new ArrayList<> ();
-        for (Map.Entry<String, List<String>> set :
-             map.entrySet()) {
-                anagrams.add(set.getValue());
-             }
-        return anagrams;
+        List<List<String>> result = new ArrayList<>();
+        for(Map.Entry<String, List<String>> entry : map.entrySet()) {
+            result.add(entry.getValue());
+        }
 
-    }
+        return result;
         
+    }
 }
