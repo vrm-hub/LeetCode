1c3d4f9df8d94e204eda6614a8d09fd4876d758f
Vinay Rajagopal Menon <74170290+vrm-hub@users.noreply.github.com>
2025-03-28 22:45:17 -0400

    Time: 2 ms (74.69%), Space: 61.8 MB (51.65%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int profit = 0;
+
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else {
+                profit = Math.max(profit, price - minPrice);
+            }
+        }
+
+        return profit;
+    }
+}
