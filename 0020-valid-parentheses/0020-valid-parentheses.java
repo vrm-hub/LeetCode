561fa7eaf1cb5b9348c552513b0f1e677615a616
Vinay Rajagopal Menon <74170290+vrm-hub@users.noreply.github.com>
2025-03-30 03:34:58 -0400

    Time: 3 ms (42.16%), Space: 42.2 MB (8.63%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,19 +1,20 @@
-class Solution {
+public class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
+        java.util.Map<Character, Character> closeToOpen = new java.util.HashMap<>();
+        closeToOpen.put(')', '(');
+        closeToOpen.put(']', '[');
+        closeToOpen.put('}', '{');
 
-        for(char c : s.toCharArray()) {
-            if (c == '(' || c == '{' || c == '[') {
-                stack.push(c);
-            } else {
-                if (stack.isEmpty()) return false;
-
-                char top = stack.pop();
-                if ((c == ')' && top != '(') ||
-                    (c == '}' && top != '{') ||
-                    (c == ']' && top != '[')) {
+        for (char c : s.toCharArray()) {
+            if (closeToOpen.containsKey(c)) {
+                if (!stack.isEmpty() && stack.peek() == closeToOpen.get(c)) {
+                    stack.pop();
+                } else {
                     return false;
                 }
+            } else {
+                stack.push(c);
             }
         }
         return stack.isEmpty();
