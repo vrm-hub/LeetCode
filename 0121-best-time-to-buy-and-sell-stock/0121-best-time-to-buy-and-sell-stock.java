0df1a8382764f176630f267b64b8d0724462bf49
Vinay Rajagopal Menon <74170290+vrm-hub@users.noreply.github.com>
2025-03-29 23:01:18 -0400

    Time: 2 ms (74.69%), Space: 61.7 MB (63.41%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -2,13 +2,6 @@ class Solution {
     public int maxProfit(int[] prices) {
         Boolean t = true;;
         int profit = 0;
-        for(int i = 1; i < prices.length; i++) {
-            if(prices[i] > prices[i-1]) {
-                t = false;
-            }
-        }
-
-        if (t) return 0;
 
         for(int i = 0 , j = 1; j < prices.length;) {
             if(prices[i] < prices[j]) {
