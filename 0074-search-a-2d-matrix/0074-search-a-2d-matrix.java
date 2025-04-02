55a04186df2fd80ce271ccbcd7fa3620f7e35883
Vinay Rajagopal Menon <74170290+vrm-hub@users.noreply.github.com>
2025-04-02 00:57:49 -0400

    Time: 0 ms (100%), Space: 42.6 MB (18.18%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,14 +1,12 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
        // 4(i-1) + j
-       int i;
        int j = matrix[0].length - 1;
-       int l = 0;
-       int r;
 
 
-       for(i = 0; i < matrix.length; i++) {
+       for(int i = 0; i < matrix.length; i++) {
         if(target >= matrix[i][0] && target <= matrix[i][j]) {
+            int l = 0;
             while(l <= j){
                 int mid = (l + j)/2;
                 if(target < matrix[i][mid]) {
