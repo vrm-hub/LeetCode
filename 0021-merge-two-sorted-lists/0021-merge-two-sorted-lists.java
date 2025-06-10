c3f2219f20224ea9490d60035a50716309860332
Vinay Rajagopal Menon <74170290+vrm-hub@users.noreply.github.com>
2025-06-10 13:51:59 -0400

    Time: 0 ms (100%), Space: 42.5 MB (59.6%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -8,23 +8,31 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if(list2 == null) {
-            return list1;
+        ListNode list3 = new ListNode();
+        ListNode temp = list3;
+
+        while (list1 != null && list2 != null) {
+            if(list1.val <= list2.val) {
+                temp.next = list1;
+                list1 = list1.next;
+            } else {
+                temp.next = list2;
+                list2 = list2.next;
+            }
+
+            temp = temp.next;
         }
 
-        if(list1 == null) {
-            return list2;
+        if(list1 != null) {
+            temp.next = list1;
+        } 
+
+        if(list2 != null) {
+            temp.next = list2;
         }
 
-        if(list1.val <= list2.val) {
-            list1.next = mergeTwoLists(list1.next, list2);
-            return list1;
-        } else {
-            list2.next = mergeTwoLists(list2.next, list1);
-            return list2;
-        }
+        return list3.next;
     }
 }
