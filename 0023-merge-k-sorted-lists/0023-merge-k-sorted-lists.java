2f9588563e21e04c519ce20e8d19202b454dbed3
Vinay Rajagopal Menon <74170290+vrm-hub@users.noreply.github.com>
2025-06-13 18:02:10 -0400

    Time: 173 ms (5.02%), Space: 44.7 MB (28.89%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    
+    private ListNode merge(ListNode l1, ListNode l2) {
+        if(l1 == null) {
+            return l2;
+        }
+
+        if(l2 == null) {
+            return l1;
+        }
+
+        if(l1.val <= l2.val) {
+            l1.next = merge(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = merge(l2.next, l1);
+            return l2;
+        }
+    }
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists.length == 0) {
+            return null;
+        }
+
+        ListNode l = lists[0];
+        for(int i = 1; i < lists.length; i++) {
+            l = merge(l, lists[i]);
+        }
+
+        return l;
+    }
+}
