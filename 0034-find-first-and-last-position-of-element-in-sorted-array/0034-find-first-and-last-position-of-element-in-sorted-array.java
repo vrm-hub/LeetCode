7ef561c33ce41696dc8e70d5e62cb9667d7ee2fc
Vinay Rajagopal Menon <74170290+vrm-hub@users.noreply.github.com>
2025-06-21 04:09:20 -0400

    Time: 0 ms (100%), Space: 46.6 MB (8.55%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,47 +1,43 @@
 class Solution {
-    private int binarySearch(int l, int r, int[] nums, int target) {
+    private int lbinarySearch(int l, int r, int[] nums, int target) {
         if(l > r) {
             return -1;
         }
 
         int mid = (l + r) / 2;
         if(nums[mid] > target) {
-            return binarySearch(l, mid -1, nums, target);
+            return lbinarySearch(l, mid -1, nums, target);
         } else if(nums[mid] < target) {
-            return binarySearch(mid + 1, r, nums, target);
+            return lbinarySearch(mid + 1, r, nums, target);
+        }
+
+        if(mid > 0 && nums[mid - 1] == target) {
+            return lbinarySearch(l, mid - 1, nums, target);
+        }
+
+        return mid;
+    }
+
+    private int rbinarySearch(int l, int r, int[] nums, int target) {
+        if(l > r) {
+            return -1;
+        }
+
+        int mid = (l + r) / 2;
+        if(nums[mid] > target) {
+            return rbinarySearch(l, mid -1, nums, target);
+        } else if(nums[mid] < target) {
+            return rbinarySearch(mid + 1, r, nums, target);
+        }
+
+        if(mid < nums.length - 1 && nums[mid + 1] == target) {
+            return rbinarySearch(mid + 1, nums.length, nums, target);
         }
 
         return mid;
     }
 
     public int[] searchRange(int[] nums, int target) {
-        int[] res = new int[]{-1, -1};
-        int pos = binarySearch(0, nums.length - 1, nums, target);
-        if(pos == -1) {
-            return res;
-        }
-        int lpos = pos;
-        int rpos = pos;
-        Boolean lflag = true, rflag = true;
-
-        while(lflag) {
-            int temp = lpos;
-            lpos = binarySearch(0, lpos - 1, nums, target);
-            if(lpos == -1) {
-                lflag = false;
-                lpos = temp;
-            }
-        }
-
-        while(rflag) {
-            int temp = rpos;
-            rpos = binarySearch(rpos + 1, nums.length - 1, nums, target);
-            if(rpos == -1) {
-                rflag = false;
-                rpos = temp;
-            }
-        }
-
-        return new int[]{lpos, rpos};
+        return new int[]{lbinarySearch(0, nums.length - 1, nums, target), rbinarySearch(0, nums.length - 1, nums, target)};
     }
 }
