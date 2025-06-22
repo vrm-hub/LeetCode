bbc511f6a51d65d6a322cdb5f4d9393c0f63496a
Vinay Rajagopal Menon <74170290+vrm-hub@users.noreply.github.com>
2025-06-22 00:17:40 -0400

    Time: 83 ms (13.06%), Space: 45.2 MB (6.72%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -8,37 +8,44 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-
 class Solution {
-    
     private ListNode merge(ListNode l1, ListNode l2) {
-        if(l1 == null) {
-            return l2;
+        ListNode head = new ListNode();
+        ListNode curr = head;
+
+        while(l1 != null && l2 != null) {
+            if(l1.val < l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+                curr = curr.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+                curr = curr.next;
+            }
         }
 
-        if(l2 == null) {
-            return l1;
+        if(l1 != null) {
+            curr.next = l1;
+            l1 = l1.next;
+            curr = curr.next;
         }
 
-        if(l1.val <= l2.val) {
-            l1.next = merge(l1.next, l2);
-            return l1;
-        } else {
-            l2.next = merge(l2.next, l1);
-            return l2;
+        if(l2 != null) {
+            curr.next = l2;
+            l2 = l2.next;
+            curr = curr.next;
         }
+
+        return head.next;
     }
-
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists.length == 0) {
-            return null;
-        }
+        ListNode res = lists.length == 0 ? null : lists[0];
 
-        ListNode l = lists[0];
         for(int i = 1; i < lists.length; i++) {
-            l = merge(l, lists[i]);
+            res = merge(res, lists[i]);
         }
 
-        return l;
+        return res;
     }
 }
