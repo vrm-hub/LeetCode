0c497c06da12eeb2af0bc68852bebab71ce41aa7
Vinay Rajagopal Menon <74170290+vrm-hub@users.noreply.github.com>
2025-06-24 04:57:29 -0400

    Time: 2 ms (68.2%), Space: 42.4 MB (75.67%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,12 +1,12 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        Queue<Pair<Integer, Integer>> q = new LinkedList<>();
+        Queue<int[]> q = new LinkedList<>();
         int freshCounter = 0;
         int counter = 0;
         for(int i = 0; i < grid.length; i++) {
             for(int j = 0; j < grid[i].length; j++) {
                 if(grid[i][j] == 2) {
-                    q.offer(new Pair<>(i, j));
+                    q.offer(new int[]{i, j});
                 }
 
                 if(grid[i][j] == 1) freshCounter++; 
@@ -19,29 +19,29 @@ class Solution {
             int length = q.size();
             counter++;
             for(int k = 0; k < length; k++) {
-                Pair<Integer, Integer> p = q.poll();
-                int i = p.getKey();
-                int j = p.getValue();
+                int[] p = q.poll();
+                int i = p[0];
+                int j = p[1];
                 if(j > 0 && grid[i][j-1] == 1) {
                     grid[i][j-1] = 2;
                     freshCounter--;
-                    q.offer(new Pair<>(i, j-1));
+                    q.offer(new int[]{i, j-1});
                 }
                 if(j < grid[i].length - 1 && grid[i][j+1] == 1) {
                     grid[i][j+1] = 2;
                     freshCounter--;
-                    q.offer(new Pair<>(i, j+1));
+                    q.offer(new int[]{i, j+1});
                 }
 
                 if(i > 0 && grid[i - 1][j] == 1) {
                     grid[i - 1][j] = 2;
                     freshCounter--;
-                    q.offer(new Pair<>(i-1, j));
+                    q.offer(new int[]{i-1, j});
                 }
                 if(i < grid.length - 1 && grid[i + 1][j] == 1) {
                     grid[i + 1][j] = 2;
                     freshCounter--;
-                    q.offer(new Pair<>(i+1, j));
+                    q.offer(new int[]{i+1, j});
                 }
             }
         }
