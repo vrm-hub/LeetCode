2b4a23791e2424bc8d22d2f2bd76315dffcd2ec5
Vinay Rajagopal Menon <74170290+vrm-hub@users.noreply.github.com>
2025-07-16 17:48:09 -0400

    Time: 49 ms (92.77%), Space: 51.6 MB (13.63%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,54 +1,43 @@
 class Solution {
-    HashMap<String, List<String>> map = new HashMap<>();
-    HashSet<String> visited = new HashSet<>();
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        wordList.add(beginWord);
-        int n = wordList.size();
-        int m = wordList.get(0).length();
-        
         if (!wordList.contains(endWord)) return 0;
 
-        for(int i = 0 ; i < n; i++) {
-            map.putIfAbsent(wordList.get(i), new ArrayList<>());
-        }
+        int L = beginWord.length();
+        Map<String, List<String>> patternMap = new HashMap<>();
 
-        for(int i = 0; i < n; i++) {
-            for(int j = i+1; j < n; j++) {
-                int cnt = 0;
-                for(int k = 0; k < m; k++) {
-                    if(wordList.get(i).charAt(k) == wordList.get(j).charAt(k)) continue;
-                    cnt++;
-                }
-                if(cnt == 1) {
-                    map.get(wordList.get(i)).add(wordList.get(j));
-                    map.get(wordList.get(j)).add(wordList.get(i));
-                }
+        // Step 1: Build the pattern map
+        for (String word : wordList) {
+            for (int i = 0; i < L; i++) {
+                String pattern = word.substring(0, i) + '*' + word.substring(i + 1);
+                patternMap.computeIfAbsent(pattern, x -> new ArrayList<>()).add(word);
             }
         }
 
-        if(map.get(endWord).size() == 0) return 0;
-
+        // Step 2: BFS
         Queue<String> q = new LinkedList<>();
+        Set<String> visited = new HashSet<>();
         q.offer(beginWord);
         visited.add(beginWord);
         int steps = 1;
 
-        while(!q.isEmpty()) {
-            int len = q.size();
-            for(int i = 0; i < len; i++) {
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
                 String word = q.poll();
                 if (word.equals(endWord)) return steps;
-                for(String w : map.get(word)) {
-                    if(!visited.contains(w)) {
-                        q.offer(w);
-                        visited.add(w);
+
+                for (int j = 0; j < L; j++) {
+                    String pattern = word.substring(0, j) + '*' + word.substring(j + 1);
+                    for (String nei : patternMap.getOrDefault(pattern, new ArrayList<>())) {
+                        if (!visited.contains(nei)) {
+                            visited.add(nei);
+                            q.offer(nei);
+                        }
                     }
                 }
             }
             steps++;
         }
-
-
 
         return 0;
     }
