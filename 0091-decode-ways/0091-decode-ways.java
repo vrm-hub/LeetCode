9e26d3ef932943c89d4d47d5965f49fb9e263934
Vinay Rajagopal Menon <74170290+vrm-hub@users.noreply.github.com>
2025-07-23 16:40:09 -0400

    Time: 1 ms (82.93%), Space: 42.3 MB (11.1%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,26 +1,38 @@
-public class Solution {
-    
+class Solution {
+    private int dfs(String s, int i, Map<Integer, Integer> dp) {
+        if(dp.containsKey(i)) {
+            return dp.get(i);
+        }
+        if(s.charAt(i) == '0') {
+            return 0;
+        }
+
+        int res = dfs(s, i + 1, dp);
+        if(i < s.length() - 1 && (s.charAt(i) == '1' || 
+               (s.charAt(i) == '2' && s.charAt(i + 1) < '7'))) {
+                res += dfs(s, i + 2, dp);
+        }
+
+        dp.put(i, res);
+        return res;
+    }
+
     public int numDecodings(String s) {
         Map<Integer, Integer> dp = new HashMap<>();
         dp.put(s.length(), 1);
 
         return dfs(s, 0, dp);
     }
-
-    private int dfs(String s, int i, Map<Integer, Integer> dp) {
-        if (dp.containsKey(i)) {
-            return dp.get(i);
-        }
-        if (s.charAt(i) == '0') {
-            return 0;
-        }
-
-        int res = dfs(s, i + 1, dp);
-        if (i + 1 < s.length() && (s.charAt(i) == '1' || 
-           s.charAt(i) == '2' && s.charAt(i + 1) < '7')) {
-            res += dfs(s, i + 2, dp);
-        }
-        dp.put(i, res);
-        return res;
-    }
 }
+
+/*
+
+12326
+      
+                    root
+                  1                                     12
+            2           23                      3      
+        3           2       26              2       26
+    2       26   6                      6
+6
+*/
