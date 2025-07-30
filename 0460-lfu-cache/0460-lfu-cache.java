422a88796c63a62591a79fb871545e20b468ebed
Vinay Rajagopal Menon <74170290+vrm-hub@users.noreply.github.com>
2025-07-30 13:42:57 -0400

    Time: 51 ms (91.76%), Space: 118.3 MB (98.48%) - LeetHub

--- 0460-lfu-cache/0460-lfu-cache.java
+++ 0460-lfu-cache/0460-lfu-cache.java
@@ -1,39 +1,45 @@
 class Node {
-    int key, val, freq;
-    Node prev, next;
+    int val, key;
+    Node next;
+    Node prev;
+    int freq;
 
-    Node(int key, int val) {
+    public Node(int key, int val) {
         this.key = key;
         this.val = val;
         this.freq = 1;
+        this.next = this.prev = null;
+    }
+
+    public int getFreq() {
+        return this.freq;
     }
 }
 
 class DoublyLinkedList {
     Node head, tail;
 
-    DoublyLinkedList() {
-        head = new Node(0, 0); // dummy head
-        tail = new Node(0, 0); // dummy tail
-        head.next = tail;
-        tail.prev = head;
+    public DoublyLinkedList() {
+        this.head = new Node(0, 0);
+        this.tail = new Node(0, 0);
+        this.head.next = this.tail;
+        this.tail.prev = this.head;
     }
 
-    void insert(Node node) {
-        // insert before tail
-        node.prev = tail.prev;
-        node.next = tail;
-        tail.prev.next = node;
-        tail.prev = node;
+    void insert(Node n) {
+        n.prev = this.tail.prev;
+        n.prev.next = n;
+        this.tail.prev = n;
+        n.next = this.tail;
     }
 
-    void remove(Node node) {
-        node.prev.next = node.next;
-        node.next.prev = node.prev;
+    void remove(Node n) {
+        n.prev.next = n.next;
+        n.next.prev = n.prev;
     }
 
     boolean isEmpty() {
-        return head.next == tail;
+        return this.head.next == this.tail;
     }
 
     Node removeLRU() {
@@ -43,52 +49,61 @@ class DoublyLinkedList {
     }
 }
 
+
 class LFUCache {
-    int cap, minFreq;
+    Map<Integer, DoublyLinkedList> dllMap = new HashMap<>();
+    int minFreq, cap;
     Map<Integer, Node> keyToNode = new HashMap<>();
-    Map<Integer, DoublyLinkedList> freqToDLL = new HashMap<>();
 
     public LFUCache(int capacity) {
-        cap = capacity;
+        this.cap = capacity;
+        this.minFreq = 0;
     }
-
+    
     public int get(int key) {
-        if (!keyToNode.containsKey(key)) return -1;
-        Node node = keyToNode.get(key);
-        update(node);
-        return node.val;
+        if(!keyToNode.containsKey(key)) return -1;
+        Node n = keyToNode.get(key);
+        update(n);
+        
+        return n.val;
     }
-
+    
     public void put(int key, int value) {
-        if (cap == 0) return;
+        if(keyToNode.containsKey(key)) {
+            Node n = keyToNode.get(key);
+            n.val = value;
+            update(n);
 
-        if (keyToNode.containsKey(key)) {
-            Node node = keyToNode.get(key);
-            node.val = value;
-            update(node);
-        } else {
-            if (keyToNode.size() == cap) {
-                // evict LRU from minFreq list
-                Node lru = freqToDLL.get(minFreq).removeLRU();
-                keyToNode.remove(lru.key);
-            }
-
-            Node node = new Node(key, value);
-            keyToNode.put(key, node);
-            freqToDLL.computeIfAbsent(1, f -> new DoublyLinkedList()).insert(node);
-            minFreq = 1;
+            return;
         }
+
+        if(keyToNode.size() == cap) {
+            Node lru = dllMap.get(minFreq).removeLRU();
+            keyToNode.remove(lru.key);
+        }
+
+        Node n = new Node(key, value);
+        keyToNode.put(key, n);
+        minFreq = 1;
+        dllMap.computeIfAbsent(minFreq, k -> new DoublyLinkedList()).insert(n);
     }
 
-    private void update(Node node) {
-        int oldFreq = node.freq;
-        freqToDLL.get(oldFreq).remove(node);
-        if (freqToDLL.get(oldFreq).isEmpty() && oldFreq == minFreq) {
-            minFreq++;
+    private void update(Node n) {
+        int freq = n.getFreq();
+        DoublyLinkedList dll = dllMap.get(freq);
+        dll.remove(n);
+        if(freq == minFreq && dll.isEmpty()) {
+                minFreq++;
         }
 
-        node.freq++;
-        freqToDLL.computeIfAbsent(node.freq, f -> new DoublyLinkedList()).insert(node);
+        n.freq = freq + 1;
+        dllMap.computeIfAbsent(n.freq, k -> new DoublyLinkedList()).insert(n);
     }
 }
 
+/**
+ * Your LFUCache object will be instantiated and called as such:
+ * LFUCache obj = new LFUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
