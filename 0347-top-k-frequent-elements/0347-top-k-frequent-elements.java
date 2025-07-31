220184df6ecb6bcaeee9294ab6021e78cf576139
Vinay Rajagopal Menon <74170290+vrm-hub@users.noreply.github.com>
2025-07-31 12:30:49 -0400

    Time: 15 ms (37.98%), Space: 49 MB (34.76%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,36 +1,20 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer, Integer> freqMap = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (!freqMap.containsKey(nums[i])) {
-                freqMap.put(nums[i], 1);
-            } else {
-                int freq = freqMap.get(nums[i]);
-                freqMap.put(nums[i], freq + 1);
-            }
+        PriorityQueue<Map.Entry<Integer, Integer>> q = new PriorityQueue<>(Collections.reverseOrder(Map.Entry.comparingByValue()));
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i : nums) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
         }
 
-        List<Integer>[] buckets = new List[nums.length + 1];
-        for (int i = 0; i < buckets.length; i++) {
-            buckets[i] = new ArrayList<>();
-        }
-        
-        for(Map.Entry<Integer,Integer> set : freqMap.entrySet()) {
-            buckets[set.getValue()].add(set.getKey());
+        for(Map.Entry<Integer, Integer> m : map.entrySet()) {
+            q.offer(m);
         }
 
-        int[] result = new int[k];
-        for(int j=0, i = buckets.length - 1; i >= 0; i--) {
-            for(Integer p : buckets[i]) {
-                result[j] = p;
-                j++;
-                if(j == k) {
-                    return result;
-                }
-            }
+        int[] res = new int[k];
+        for(int i = 0; i < k; i++) {
+            res[i] = q.poll().getKey();
         }
 
-        return result;
-
+        return res;
     }
 }
