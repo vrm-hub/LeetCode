94695f537e81b032949360eda7d3a72f37e834a9
Vinay Rajagopal Menon <74170290+vrm-hub@users.noreply.github.com>
2025-07-31 14:03:17 -0400

    Time: 1 ms (82.71%), Space: 43.7 MB (19.8%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,18 @@
+class Solution {
+    List<String> res = new ArrayList<>();
+    private void backtracking(int n, int o, int c, String s) {
+        if(o + c == 2 * n) {
+            res.add(s);
+            return;
+        }
+
+        if(o < n) backtracking(n, o + 1, c, s + "(");
+        if(o != c && c < n) backtracking(n, o, c + 1, s + ")");
+
+    }
+    public List<String> generateParenthesis(int n) {
+        backtracking(n, 1, 0, "(");
+        
+        return res;
+    }
+}
