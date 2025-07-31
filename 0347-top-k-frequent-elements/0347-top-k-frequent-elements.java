fa6013f5944606e961c8828b15375da80af92d38
Vinay Rajagopal Menon <74170290+vrm-hub@users.noreply.github.com>
2025-07-31 12:37:46 -0400

    Time: 14 ms (57.5%), Space: 49.2 MB (13.65%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        PriorityQueue<Map.Entry<Integer, Integer>> q = new PriorityQueue<>(Collections.reverseOrder(Map.Entry.comparingByValue()));
+        PriorityQueue<Map.Entry<Integer, Integer>> q = new PriorityQueue<>(Map.Entry.comparingByValue());
         HashMap<Integer, Integer> map = new HashMap<>();
         for(int i : nums) {
             map.put(i, map.getOrDefault(i, 0) + 1);
@@ -8,11 +8,12 @@ class Solution {
 
         for(Map.Entry<Integer, Integer> m : map.entrySet()) {
             q.offer(m);
+            while(q.size() > k) q.poll();
         }
 
         int[] res = new int[k];
         for(int i = 0; i < k; i++) {
-            res[i] = q.poll().getKey();
+            res[k - i - 1] = q.poll().getKey();
         }
 
         return res;
