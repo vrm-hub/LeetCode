42ec643d5f03b80ce7d5fed8f375dc80147347d8
Vinay Rajagopal Menon <74170290+vrm-hub@users.noreply.github.com>
2025-07-30 20:49:49 -0400

    Time: 1 ms (100%), Space: 46.3 MB (26.41%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,33 +1,44 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int i = 0;
-        int l = 0, r = 0;
-        int len1 = nums1.length, len2 = nums2.length;
-        int m = (len1 + len2) / 2;
-        int median1 = 0, median2 = 0;
-    
-        while(i < m + 1) {
-            median2 = median1;
-            if (l < len1 && r < len2) {
-                if (nums1[l] > nums2[r]) {
-                    median1 = nums2[r];
-                    r++;
-                } else {
-                    median1 = nums1[l];
-                    l++;
-                }
-            } else if (l < len1) {
-                median1 = nums1[l];
-                l++;
+        if(nums1.length > nums2.length) return findMedianSortedArrays(nums2, nums1);
+        int low = 0;
+        int high = nums1.length;
+        int mid = (low + high)/2;
+        while(low <= high) {
+            int mid1 = (low + high)/2;
+            int mid2 = (nums1.length + nums2.length + 1)/2 - mid1;
+            int l1, l2, r1, r2;
+            if(mid1 - 1 < 0) {
+                l1 = Integer.MIN_VALUE;
             } else {
-                median1 = nums2[r];
-                r++;
+                l1 = nums1[mid1 - 1];
             }
-            i++;
+
+            if(mid2 - 1 < 0) {
+                l2 = Integer.MIN_VALUE;
+            } else {
+                l2 = nums2[mid2 - 1];
+            }
+
+            if(mid1 >= nums1.length) {
+                r1 = Integer.MAX_VALUE;
+            } else {
+                r1 = nums1[mid1];
+            }
+
+            if(mid2 >= nums2.length) {
+                r2 = Integer.MAX_VALUE;
+            } else {
+                r2 = nums2[mid2];
+            }
+
+            if(l1 > r2) high = mid1 - 1;
+            else if(l2 > r1) low = mid1 + 1;
+            else if(l1 <= r2 && l2 <= r1) {
+                return (nums1.length + nums2.length) % 2 == 0 ? (double)(Math.max(l1, l2) + Math.min(r1, r2))/2 : (double)Math.max(l1,l2);
+            }
         }
 
-        if((len1 + len2) % 2 == 0) return (double)(median1 + median2) / 2;
-
-        return median1;
+        return 0;
     }
 }
